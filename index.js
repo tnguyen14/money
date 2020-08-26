4af8947fc97393455dd15565d853070430351252
Tri Nguyen <tri@tridnguyen.com>
2020-08-26 01:39:21 -0400

    returning number as well as string

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-export function usd(amount) {
-  if (!amount) {
-    return '$0.00';
-  } else {
-    return `$${(amount / 100).toFixed(2)}`;
+export function usd(amount, asNumber = false) {
+  let currency = 0;
+  if (amount) {
+    currency = amount / 100;
   }
+  if (asNumber) {
+    return currency
+  }
+  return `$${currency.toFixed(2)}`;
 }
 
 export function fromUsd(currency) {
